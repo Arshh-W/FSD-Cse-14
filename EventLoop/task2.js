69f785a1ff4a415e187bcb0534e2e128ecd21809
Arsh Rana <q43676709@gmail.com>
2026-07-23 15:34:45 +0530

    EventLoop

---
+++ EventLoop/task2.js
@@ -0,0 +1,34 @@
+const f1 = () => {
+  console.log("f1 starts");
+  f2();
+  console.log("f1 running");
+  console.log("f1 end");
+};
+const f2 = () => {
+  console.log("f2 starts");
+  f3();
+  console.log("f2 running");
+  console.log("f2 ends");
+};
+const f3 = () => {
+  console.log("f3 starts");
+  console.log("f3 running");
+  console.log("f3 end");
+};
+function main() {
+  console.log("main");
+  f1();
+  console.log("end main");
+}
+main();
+
+//synchronous call 
+//js is synchronous and single threaded 
+
+// in asynchronous we use event U to manage the call stack 
+
+//asynchronous call using timer
+//1. Set time out 
+//2. Set Immediate 
+//3. Process.next tick
+//4. set interval
